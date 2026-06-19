801061900f88cf94d597f6a9cff52ca5861ef701
2concrete <danielpeace654@gmail.com>
2026-06-19 16:44:18 +0800

    added powershell profile and renamed omp config

---
+++ powershell/profile.ps1
@@ -0,0 +1,16 @@
+oh-my-posh init pwsh --config "C:\Users\daniel\.config\concrete.omp.json" | Invoke-Expression
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
