35f822b95636f7772f1b8a2b64db8b01fb8988ca
2concrete <danielpeace654@gmail.com>
2026-06-20 15:35:59 +0800

    winfetch runs on terminal open

--- startup.bat
+++ startup.bat
@@ -1,7 +1,7 @@
 @echo off
 taskkill /F /IM komorebi.exe >nul 2>&1
 taskkill /F /IM whkd.exe >nul 2>&1
+start "" wt
 start /B "" komorebic start --whkd
 start /B "" yasbc start
-start "" wt
 start "" "C:\Program Files\OpenRGB\OpenRGB.exe" --startminimized
